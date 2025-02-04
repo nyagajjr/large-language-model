0083d666515b01701e2a762cde039a2a12f3d9d7
nyagajjr <nyagaben123@gmail.com>
2025-02-04 13:05:56 +0100

    adding tokens IDs

--- tokenizer.py
+++ tokenizer.py
@@ -3,6 +3,15 @@ import re
 with open('the-verdict.txt', 'r', encoding='utf-8') as file:
     raw_data = file.read()
 
+#creating tokens
 split_data = re.split(r'([,.:;?!]|--|\s)', raw_data)
 data = [item for item in split_data if item.strip()]
-print(data[:200])
+#print(data[:200])
+
+#assigning tokens ids
+sorted_data = sorted(set(data))
+vocabulary = {}
+for integer,token in enumerate(sorted_data):
+    vocabulary[integer]=token
+
+print(vocabulary)
