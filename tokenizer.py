dd783531b49fe52b9f95445ccf77c408f086a6fb
nyagajjr <nyagaben123@gmail.com>
2025-02-06 13:02:40 +0100

    adding the decode function

--- tokenizer.py
+++ tokenizer.py
@@ -19,6 +19,8 @@ for index,token in enumerate(sorted_data):
 class simple_tokenizer_v1:
     def __init__(self, tokens):
         self.tokens = tokens
+        self.token_id = {index:token for token, index in vocabulary.items()}
+
 
     def tokens_ids(self, raw_data):
         split_data = re.split(r'([,.:;?_!"()\']|--|\s)', raw_data)
@@ -28,9 +30,18 @@ class simple_tokenizer_v1:
                 data.append(item.strip())
         tokens_ids = [self.tokens[token_id] for token_id in data]
         return tokens_ids
+    
+    
+    def decode(self, tokens_ids):
+        text = " ".join([self.token_id[i] for i in tokens_ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
 
 tokenizer = simple_tokenizer_v1(vocabulary)
 txt1 = '''"It's the last he painted, you know,"'''
 token_id = tokenizer.tokens_ids(txt1)
 print(token_id)
+
+id_to_text = tokenizer.decode(token_id)
+print(id_to_text)
         
